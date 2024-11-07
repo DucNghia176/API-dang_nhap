1ea00b59441624aa0625df79f6f59b80adaf56a7
DucNghia176 <nghi7a@gmail.com>
2024-11-08 01:21:33 +0700

    product: add, delete, update, creat

---
+++ product/src/main/java/entity/Product.java
@@ -0,0 +1,27 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    String productID;   //id sp
+    String productName; //tên sp
+    Float price;    //giá
+    int stock;  //sl tồn kho
+    String category; //loại sp
+
+}
